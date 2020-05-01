35421858cb3c9e176734b534b7aababeccf5cfbd
Jiancao164 <60164697+Jiancao164@users.noreply.github.com>
2020-05-01 12:12:42 -0700

    change endpoint

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,2 +1,2 @@
-export const HEROKU_SERVER_URL = "http://localhost:8080/api";
-//export const HEROKU_SERVER_URL = "https://fathomless-atoll-86606.herokuapp.com";
+//export const HEROKU_SERVER_URL = "http://localhost:8080/api";
+export const HEROKU_SERVER_URL = "https://intense-beach-38233.herokuapp.com";
